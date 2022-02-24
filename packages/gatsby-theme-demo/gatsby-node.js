344d344e8367d9143011850954ebd1f776eba459
Stephen Merth <smerth@users.noreply.github.com>
2022-02-24 13:33:30 -0800

    Update gatsby-node.js

--- packages/gatsby-theme-demo/gatsby-node.js
+++ packages/gatsby-theme-demo/gatsby-node.js
@@ -1,7 +1,9 @@
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
-  const homepage = require.resolve(
-    `${__dirname}/src/templates/Homepage.js`,
+  const homepage = require.resolve(`${__dirname}/src/templates/Homepage.js`);
+  const pageSlug = "second-page";
+  const secondpage = require.resolve(
+    `${__dirname}/src/templates/Secondpage.js`,
   );
 
   // Create a single page.
@@ -9,6 +11,17 @@ exports.createPages = ({ actions }) => {
     path: `/`, // Path for this page — required
     component: homepage,
     context: {
+      pageSlug,
+      // Add optional context data to be inserted
+      // as props into the page component..
+    },
+  });
+  // Create a single page.
+  createPage({
+    path: pageSlug, // Path for this page — required
+    component: secondpage,
+    context: {
+      pageSlug,
       // Add optional context data to be inserted
       // as props into the page component..
     },
